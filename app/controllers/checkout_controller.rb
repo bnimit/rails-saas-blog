d5473e77bea98c3e6b18258b4ce0090b6891d1fa
Nimit Bhandari <nimitbhandari@outlook.com>
2024-02-05 20:29:16 +0530

    Attempt to fix link_to method post issue

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,17 @@
+class CheckoutController < ApplicationController
+    def create
+        @session = Stripe::Checkout::Session.create({
+            success_url: posts_url,
+            cancel_url:  pricing_url,
+            payment_method_types: ['card'],
+            line_items: [
+                { price: 'price_1OgKFySElIXro00wzt6zGHki', quantity: 1 }
+            ],
+            mode: 'subscription'
+        })     
+        
+        respond_to do |format|
+            format.js
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "pricing", to: "static_pages#pricing"
   root "posts#index"
   resources :posts
+  post "checkout/create", to: "checkout#create", as: "checkout_create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
